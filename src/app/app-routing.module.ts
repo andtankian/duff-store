ff94850345cf81a5fa28c130a1be5f11848a2af8
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-28 20:41:59 -0300

    Criei nova página, botão de navegação e implementação do constructor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'puro-malte', loadChildren: './puro-malte/puro-malte.module#PuroMaltePageModule' },
+  { path: 'pilsen', loadChildren: './pilsen/pilsen.module#PilsenPageModule' },
+  { path: 'cerveja-detalhes', loadChildren: './cerveja-detalhes/cerveja-detalhes.module#CervejaDetalhesPageModule' },
 ];
 
 @NgModule({
